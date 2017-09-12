d340ca657227eea44b7d89d872c5ba9b75b2251e
ryandeussing <ryan@ryandeussing.com>
2017-09-12 16:17:32 -0400

    img caption

--- src/assets/javascript/index.js
+++ src/assets/javascript/index.js
@@ -10955,7 +10955,7 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 /*!
- * Flickity PACKAGED v2.0.9
+ * Flickity PACKAGED v2.0.10
  * Touch, responsive, flickable carousels
  *
  * Licensed GPLv3 for open source use
@@ -13029,7 +13029,7 @@ proto._bindStartEvent = function( elem, isBind ) {
   isBind = isBind === undefined ? true : !!isBind;
   var bindMethod = isBind ? 'addEventListener' : 'removeEventListener';
 
-  if ( false ) {
+  if ( window.PointerEvent ) {
     // Pointer Events. Chrome 55, IE11, Edge 14
     elem[ bindMethod ]( 'pointerdown', this );
   } else {
@@ -13701,20 +13701,16 @@ proto.pointerDownFocus = function( event ) {
   }
 };
 
-var touchStartEvents = {
-  touchstart: true,
-  pointerdown: true,
-};
-
 var focusNodes = {
   INPUT: true,
   SELECT: true,
 };
 
 function getCanPointerDown( event ) {
-  var isTouchStart = touchStartEvents[ event.type ];
+  var isTouchStart = event.type == 'touchstart';
+  var isTouchPointer = event.pointerType == 'touch';
   var isFocusNode = focusNodes[ event.target.nodeName ];
-  return isTouchStart || isFocusNode;
+  return isTouchStart || isTouchPointer || isFocusNode;
 }
 
 proto.canPreventDefaultOnPointerDown = function( event ) {
@@ -14960,7 +14956,7 @@ return Flickity;
 }));
 
 /*!
- * Flickity v2.0.9
+ * Flickity v2.0.10
  * Touch, responsive, flickable carousels
  *
  * Licensed GPLv3 for open source use
